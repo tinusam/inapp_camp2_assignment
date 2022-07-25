495c5547053a76cb24e4d03344ad19e2b10f09c6
tinusam <tinu.s@inapp.com>
2022-07-25 16:05:16 +0530

    done

--- python/calculator.py
+++ python/calculator.py
@@ -1,7 +1,5 @@
 from abc import ABC, abstractmethod
 
-
-
 class calculator(ABC):
     def __init__(self):
         self.__num1 = None
@@ -39,10 +37,39 @@ class CalcProd(calculator):
 
 class CalcQuo(calculator):
     def calculate(self):
-        return self.num2 / self.num1
+        return self.num2 // self.num1
 
 
-sum = CalcSum()
-sum.num1 = 9
-sum.num2 = 5
-print(sum.calculate())
+option = 0
+while option != 5:
+    print("Calculator:\n 1.Add\n 2.Subtract\n 3.Product\n 4.Division\n 5.Exit")
+    option = int(input("enter your option: "))
+    if option == 1:
+        sum = CalcSum()
+        sum.num1 = int(input("Enter first number:"))
+        sum.num2 = int(input("Enter second number:"))
+        print(sum.calculate())
+
+    elif option == 2:
+        sub = CalcDiff()
+        sub.num1 = int(input("Enter a number:"))
+        sub.num2 = int(input("Enter a number:"))
+        print(sub.calculate())
+
+    elif option == 3:
+        prod = CalcProd()
+        prod.num1 = int(input("Enter a number:"))
+        prod.num2 = int(input("Enter a number:"))
+        print(prod.calculate())
+
+    elif option == 4:
+        quo = CalcQuo()
+        quo.num1 = int(input("Enter a number:"))
+        quo.num2 = int(input("Enter a number:"))
+        print(quo.calculate())
+
+    elif option == 5:
+        pass
+        
+    else:
+        print("wrong option")
